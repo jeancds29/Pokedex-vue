87cb75f586281a3f3b8ef607c80cbab66203a5d4
Jean Cerqueira <74473537+jeancds29@users.noreply.github.com>
2021-06-06 00:11:53 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id='app'>
+    <div class="column is-half is-offset-one-quarter">
+      <h1 class="is-size-1">Pokedex</h1>
+      <hr>
+      <input type="text" class="input is-rounded" placeholder="buscar pokemon pelo nome" v-model="busca">
+      <button class="button is-primary is-rounded" id="btnBusca" @click="buscar">Buscar</button>
+
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
+        <pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
+    </div>
+    
+  </div>
+  
+</template>
+
+<script>
+
+import axios from 'axios'
+import Pokemon from './components/Pokemon.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
+    }
+  },
+  created: function() {
+    axios.get(" https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+      this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
+    })
+  },
+  components:{
+    Pokemon
+  },
+
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+  /*computed: {
+    
+    resultadoBusca: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+   
+  } */
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  text-transform:capitalize;
+}
+#btnBusca{
+  margin-top: 1%;
+}
+</style>
